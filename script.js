d0106a4d1dcbca0541b22f6fc03543c556f92d4c
ToYu200 <subota300716@gmail.com>
2025-01-25 23:23:12 +0300

    V2
    Сегодня офигенно поработал. Начал уже думать над логикой, доделал CSS, хорошо начал изучать работу с DOM. Только вверх!

--- script.js
+++ script.js
@@ -6,5 +6,29 @@
 // function newTask(){
 
 // }
+let result = []
+function reader(){
+    var input = document.querySelector('input').value
+    let  = result.push(input)
+    console.log(result)
+}
 
+function cleanSearch(){
+    document.querySelector('input').value = ''
+}
 
+function createTask(){
+
+}
+let li = document.createElement('li');
+li.innerHTML += '<div>Hi</div>';
+
+// чтобы сниппент что-нибудь отобразил
+document.body.appendChild(li);
+
+document.addEventListener( 'keyup', event => {
+    if( event.code === 'Enter' ){
+        reader();
+        cleanSearch();  
+    };
+  });
